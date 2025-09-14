14d8100b077cf5443378857c02689a93ee2f7c93
Shefalidevops <shefali25yt@gmail>
2025-09-13 23:14:05 -0400

    nginx part done

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
 # Importing flask module in the project is mandatory
 # An object of Flask class is our WSGI application.
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from datetime import datetime
+# from urllib import request
 import argparse
+import os
+
+env = os.getenv("environment")
 
 # Flask constructor takes the name of 
 # current module (__name__) as argument.
@@ -14,21 +18,33 @@ app = Flask(__name__)
 @app.route('/healthz')
 # ‘/’ URL is bound with hello_world() function.
 def healthz():
-    return jsonify({"status": "ok"}), 200
+    return jsonify({"status": "ok", "environment":env}), 200
 
 @app.route('/date')
 def date(): 
     today = datetime.now()
     return jsonify({"date": today}), 200
 
+
+@app.route('/write', methods = ['POST'])
+def write():
+    if request.is_json:
+        data = request.get_json()
+        print(data)
+        message = data.get("message")
+        if message:
+            with open("./logs/messages.txt", "a") as file:
+                file.write(message + "\n")
+    return "ok", 200
+
 # main driver function
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description="run flask")
-    parser.add_argument("host",type=str, help="IP Address such as '0.0.0.0'", default="0.0.0.0")
-    parser.add_argument("port",type=int, help= "port wher you want to expose the flask app",default=5000)
+    parser.add_argument("--host",required=False, type=str, help="IP Address such as '0.0.0.0'", default="127.0.0.1")
+    parser.add_argument("--port",required=False,type=int, help= "port wher you want to expose the flask app",default=5000)
     args = parser.parse_args()
     # run() method of Flask class runs the application 
     # on the local development server.
-    app.run(host=args.host,port=args.port)
-    # app.run(host="0.0.0.0")
+    app.run(host=args.host,port=args.port, debug=True)
+    # app.run()
